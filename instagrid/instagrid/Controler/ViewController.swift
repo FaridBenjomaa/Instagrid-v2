b7c8665e42dbfacc5112659d90f1a1467db995bc
farid benjomaa <farid.benjomaa@hotmail.fr>
2021-03-18 22:50:20 +0100

    Mise en place des boutons layout

--- instagrid/instagrid/Controler/ViewController.swift
+++ instagrid/instagrid/Controler/ViewController.swift
@@ -9,15 +9,49 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var selected = UIImage(named: "Selected")
+    var layout1 = UIImage(named: "Layout 1")
+    var layout2 = UIImage(named: "Layout 2")
+    var layout3 = UIImage(named: "Layout 3")
+
+    @IBOutlet weak var layoutButton1: UIButton!
+    @IBOutlet weak var layoutButton2: UIButton!
+    @IBOutlet weak var layoutButton3: UIButton!
     
+    @IBOutlet weak var view1: UIView!
+    @IBOutlet weak var view2: UIView!
+    @IBOutlet weak var view3: UIView!
+    @IBOutlet weak var view4: UIView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
     
+    @IBAction func layout1(_ sender: Any) {
+        view4.isHidden = false
+        view2.isHidden = true
+        layoutButton1.setImage(selected, for: UIControl.State.normal)
+        layoutButton2.setImage(layout2, for: UIControl.State.normal)
+        layoutButton3.setImage(layout3, for: UIControl.State.normal)
+    }
+    
+    @IBAction func layout2(_ sender: Any) {
+        view4.isHidden = true
+        view2.isHidden = false
+        layoutButton1.setImage(layout1, for: UIControl.State.normal)
+        layoutButton2.setImage(selected, for: UIControl.State.normal)
+        layoutButton3.setImage(layout3, for: UIControl.State.normal)
+    }
+    
+    @IBAction func layout3(_ sender: Any) {
+        view4.isHidden = false
+        view2.isHidden = false
+        layoutButton1.setImage(layout1, for: UIControl.State.normal)
+        layoutButton2.setImage(layout2, for: UIControl.State.normal)
+        layoutButton3.setImage(selected, for: UIControl.State.normal)
+    }
     
 }
 
